362f23c8902da43cc8aefc25b707ec91d0d32ecb
gray0108 <jiung0108@gmail.com>
2021-07-25 23:46:53 +0900

    Put clock by Date library

--- VanilaJS_ChromeApp/js/clock.js
+++ VanilaJS_ChromeApp/js/clock.js
@@ -1,8 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    // Date 객체 생성
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-//sayHello function을 매 5초(5000ms) 마다 실행
-setInterval(sayHello, 5000);
+getClock();
+//getClock function을 매 1초(1000ms) 마다 실행
+setInterval(getClock, 1000);
