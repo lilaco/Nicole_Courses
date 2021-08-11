33019fbebdfaa4ff494117d373d45edf0e83361a
gray0108 <jiung0108@gmail.com>
2021-08-11 23:49:28 +0900

    Learning about defining object

---
+++ VanilaJS_ChromeApp/js/app.js
@@ -0,0 +1,17 @@
+console.log("hi");
+
+const playerName = "zoong";
+const playerPoints = 121212;
+const playerHandsome = false;
+const playerFat = "little bit";
+
+// 입력과 속도는 빠르지만 key를 알 수 없음. value의 의미를 알기 어렵다.
+const player = ["zoong", 121212, false, "little bit"];
+
+// Object
+const player = {
+    name: "zoong", 
+    points: 121212, 
+    handsome: false, 
+    fat: "little bit"
+};
