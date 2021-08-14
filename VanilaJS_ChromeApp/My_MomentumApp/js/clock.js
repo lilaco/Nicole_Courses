5a5c9c8ab58669fc23d1c51645df3aee6f3a18bc
gray0108 <jiung0108@gmail.com>
2021-08-14 22:32:12 +0900

    Add clock.js at my app.

--- VanilaJS_ChromeApp/My_MomentumApp/js/clock.js
+++ VanilaJS_ChromeApp/My_MomentumApp/js/clock.js
@@ -1,7 +1,17 @@
 const clock = document.querySelector("#clock");
 
-function timeNow(){
+getClock();
+
+function getClock(){
     // Date 라이브러리 불러오기
     const date = new Date();
     
+    const hour = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+
+    clock.innerText = `${hour}:${minutes}:${seconds}`;
+
+    // getClock을 1초마다 실행.
+    setInterval(getClock,1000);
 }
