7f884fcf6a548c02d3516bfa8b0510749260348f
gray0108 <jiung0108@gmail.com>
2021-08-15 00:31:15 +0900

    Modified clock.js

--- VanilaJS_ChromeApp/My_MomentumApp/js/clock.js
+++ VanilaJS_ChromeApp/My_MomentumApp/js/clock.js
@@ -6,7 +6,7 @@ function getClock(){
     // Date 라이브러리 불러오기
     const date = new Date();
     
-    const hour = date.getHours();
+    const hour = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
 
