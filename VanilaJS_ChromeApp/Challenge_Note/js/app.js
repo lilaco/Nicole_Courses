cce475f1f1279bc9a49d2f84786b7d54e39d91a3
gray0108 <jiung0108@gmail.com>
2021-08-25 00:12:51 +0900

    Following 2.2 variables for challenge.

--- VanilaJS_ChromeApp/Challenge_Note/js/app.js
+++ VanilaJS_ChromeApp/Challenge_Note/js/app.js
@@ -1,1 +1,29 @@
 console.log("Hey!");
+
+/* 
+* #2.2 Variables
+*
+* Written by ZOONG
+* Created date : 2021-08-24
+*/
+
+// //const는 상수를 의미 constant
+// const a = 5;
+// const b = 2;
+
+// const myName = "Zoong"; //자바스크립트에서 변수명을 지정할때 CamelCase를 차용.
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+
+// console.log("hello " + myName);
+
+//------------------------------------------------------------------------------------
+
+/*
+* #2.3 const and let
+* 
+* Written by ZOONG
+* Created date : 2021-08-25
+*/
