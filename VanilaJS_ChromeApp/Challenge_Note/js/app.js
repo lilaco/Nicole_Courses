ffd831b37c54c3a5510c4167d58ea5944fd878fd
gray0108 <jiung0108@gmail.com>
2021-08-25 00:41:59 +0900

    Following 2.4 Booleans for challenge.

--- VanilaJS_ChromeApp/Challenge_Note/js/app.js
+++ VanilaJS_ChromeApp/Challenge_Note/js/app.js
@@ -27,20 +27,34 @@ console.log("Hey!");
 * Created date : 2021-08-25
 */
 
-let a = 5;
-let b = 2;
-let myName = "Zoong";
+// let a = 5;
+// let b = 2;
+// let myName = "Zoong";
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
 
-console.log("hello " + myName);
+// console.log("hello " + myName);
 
-myName = "Jiwoong";
+// myName = "Jiwoong";
 
-console.log("Your name is " + myName); // let으로 변수를 선언했기 때문에 값을 update 할 수있다. const라면 TypeError 발생.
+// console.log("Your name is " + myName); // let으로 변수를 선언했기 때문에 값을 update 할 수있다. const라면 TypeError 발생.
 
-// 니꼬의 경험에 따르면, const를 기본적으로 사용하고, 만약 variable을 업데이트 하고 싶다면 let을 사용한다. 
-// Always const, sometimes let, never var!
+// // 니꼬의 경험에 따르면, const를 기본적으로 사용하고, 만약 variable을 업데이트 하고 싶다면 let을 사용한다. 
+// // Always const, sometimes let, never var!
 //------------------------------------------------------------------------------------
+
+/*
+* #2.4 Booleans
+*
+* Written by ZOONG
+* Created date : 2021-08-25
+*/
+
+// const amIFat = null;
+// console.log(amIFat);
+
+// let something;
+// console.log(something);
+// //------------------------------------------------------------------------------------
